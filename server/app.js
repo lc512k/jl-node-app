42df5668e3663cbe355310529c601f0f2a536e19
James Loveridge <dev@j-m.es>
2018-07-06 22:00:54 +0100

    Add mongoose lib

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const someController = require('./controllers/some-controller');
 const anotherController = require('./controllers/another-controller');
