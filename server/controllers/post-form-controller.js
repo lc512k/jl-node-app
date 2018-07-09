081ceff32c2d1f904317b009ddaa4c3e725744eb
James Loveridge <dev@j-m.es>
2018-07-09 22:16:13 +0100

    Save responses from form to DB

--- server/controllers/post-form-controller.js
+++ server/controllers/post-form-controller.js
@@ -5,6 +5,19 @@ const main = (req, res) => {
 	console.log('country:', country);
 	console.log('gdpr:', gdpr);
 
+	const formData = require('./../models/form');
+	const newFormInput = new formData({
+		name: name,
+		email: email,
+		country: country,
+		gdpr: gdpr
+	});
+
+	newFormInput.save((error) => {
+		if (error) throw error;
+		console.log('New form data inputted!');
+	});
+
 	res.render('form-result', {
 		title: 'Success',
 		pageTitle: 'Your submission was successful!',
