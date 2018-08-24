5ecee0e88969aaff0142f67e018f3db562e4aa09
James Loveridge <dev@j-m.es>
2018-08-24 13:07:29 +0100

    Another iteration of fetch

--- server/controllers/get-ft-headlines.js
+++ server/controllers/get-ft-headlines.js
@@ -1,29 +1,33 @@
+const fetch = require('isomorphic-fetch');
 const main = (req, res) => {
 
 	// process.env.FT_API = key
-	const fetch = require('isomorphic-fetch');
-	let options = {
+	const options = {
 		method: 'POST',
-		body: {
+		headers: {
+			'X-Api-Key': process.env.FT_API,
+			'Content-Type': 'application/json',
+			// 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+		},
+		body: JSON.stringify({
 			'queryString': 'banks',
 			'resultContext': {
 				'aspects': ['title', 'lifecycle', 'location', 'summary', 'editorial']
 			}
-		},
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded',
-			'X-Api-Key': process.env.FT_API,
-		}
+		})
 	};
 
 	fetch('http://api.ft.com/content/search/v1', options)
 	.then((res) => {
-		console.log('🔔 =>', res);
+		if (res.status === 400) {
+			throw new Error(res.status + ' - ' + res.statusText);
+		}
+		console.log('🔔 ', res);
 	})
 	.catch((error) => {
-		throw new Error(error);
+		console.log('⛔️ ', error);
 	});
-
+	
 	res.render('headlines', {
 
 	});
